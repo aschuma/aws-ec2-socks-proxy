af39f3bf32c7c39bb87faf5d6a8642e7e2b2e9c0
aschuma <nusers.noreply.github.com>
2024-05-10 12:26:43 +0200

    Fix auto ami selection - part 2

--- amiImages.py
+++ amiImages.py
@@ -16,14 +16,24 @@ def awssocks_ami_image():
         for page in ami_paginator.paginate(Path="/aws/service/ami-amazon-linux-latest"):
             ami_params += page["Parameters"]
 
-        amzn2_amis = [ap for ap in ami_params if
-                      all(query in ap['Name'] for query
-                          in ('amzn2', AWSSOCKS_EC2_ARCHITECTURE, 'gp2'))]
+        amzn2_amis_any_arch = [ap for ap in ami_params if
+                               all(query in ap['Name'] for query
+                                   in ('amzn2', 'gp2'))]
+
+        amzn2_amis = [ap for ap in amzn2_amis_any_arch if
+                      ap['Name'] in AWSSOCKS_EC2_ARCHITECTURE]
+
         if len(amzn2_amis) > 0:
             ami_image_id = amzn2_amis[0]['Value']
             logger.info("Found an Amazon Machine Image (AMI) that includes Amazon Linux 2, "
                         "an x64 architecture, and a general-purpose EBS volume: %s",
                         str(amzn2_amis[0]))
+            return ami_image_id
+        elif len(amzn2_amis_any_arch) > 0:
+            ami_image_id = amzn2_amis_any_arch[0]['Value']
+            logger.info("Found an Amazon Machine Image (AMI) that includes Amazon Linux 2, "
+                        "and a general-purpose EBS volume: %s",
+                        str(amzn2_amis_any_arch[0]))
             return ami_image_id
         elif len(ami_params) > 0:
             ami_image_id = ami_params[0]['Value']
